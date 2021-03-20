922aa1808dcadd450cd1f1a1cae866620c48c2b2
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2021-03-20 12:34:56 -0400

    call api through load balancer

---
+++ App/Api/Controllers/IncrementController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class IncrementController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "hello increment";
+        }
+
+
+    }
+}
+
