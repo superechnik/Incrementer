0cc9c2b172a1a63bdbe0576d3cd690849a7738f1
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2021-03-20 14:46:25 -0400

    queue setup

--- App/Api/Controllers/IncrementController.cs
+++ App/Api/Controllers/IncrementController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Lib;
+using MassTransit;
 
 namespace Api.Controllers
 {
@@ -12,14 +13,22 @@ namespace Api.Controllers
     [ApiController]
     public class IncrementController : ControllerBase
     {
+        readonly IBus _bus;
+
+        public IncrementController(IBus bus)
+        {
+            _bus = bus;
+        }
+
 
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Lib.KeyValuePair kvp)
         {
+            var endPoint = await _bus.GetSendEndpoint(new Uri("queue:input-queue"));
+            await endPoint.Send(kvp); 
             return Ok();
+
         }
-
-
     }
 }
 
--- App/Api/Startup.cs
+++ App/Api/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MassTransit;
 
 namespace Api
 {
@@ -26,8 +27,25 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMassTransit(x =>
+            {
+                x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(config =>
+                {
+                    config.UseHealthCheck(provider);
+                    config.Host("rabbit", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                }));
+            });
+
+            services.AddMassTransitHostedService();
 
             services.AddControllers();
+
+            
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
