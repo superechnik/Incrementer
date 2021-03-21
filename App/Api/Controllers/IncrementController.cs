a64e42c1f820983eae73b974a9e09a1af1890362
Matthew Rosenberg <rosenberg.matthew@gmail.com>
2021-03-21 11:53:27 -0400

    update api post method

--- App/Api/Controllers/IncrementController.cs
+++ App/Api/Controllers/IncrementController.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Lib;
 using MassTransit;
 
 namespace Api.Controllers
@@ -20,15 +17,46 @@ namespace Api.Controllers
             _bus = bus;
         }
 
-
-        [HttpPost]
+    /// <summary>
+    /// Accepts body of {Key:string, Value:decimal} and sends the 
+    /// body into the rabbitMq queue
+    /// </summary>
+    /// <param name="kvp"></param>
+    /// <returns></returns>
+    [HttpPost]
         public async Task<IActionResult> Post([FromBody] Lib.KeyValuePair kvp)
         {
-            var endPoint = await _bus.GetSendEndpoint(new Uri("queue:kvpQueue"));
-            await endPoint.Send(kvp); 
-            return Ok();
 
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Please check you request body, something is not correct");
+            }
+            else if (kvp.Value < 0)
+            {
+                return BadRequest("Did you mean /Decrement ?");
+            }
+            else if (kvp.Value == 0)
+            {
+                return Ok("This endpoint does not increment by 0");
+            }
+
+            var endPoint = await _bus.GetSendEndpoint(new Uri("queue:kvpQueue"));
+
+            try
+            {
+                await endPoint.Send(kvp);
+
+                return Ok($"Value {kvp.Value} has been queued for key: {kvp.Key}");
+            }
+
+            catch (Exception ex)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, ex);
+            }
         }
+
+
     }
 }
 
